9c6ecfc59b2fa01569ff886274a20129eab4bc76
AndyGar04 <andygarciaprost2004@gmail.com>
2025-06-14 11:17:55 -0300

    Add: initial Front

--- TP4/Backend/app.js
+++ TP4/Backend/app.js
@@ -3,6 +3,9 @@ const app = express();
 const personaRoutes = require('./routes/personas.routes');
 const PORT = 3000;
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 app.use('/', personaRoutes);
 
